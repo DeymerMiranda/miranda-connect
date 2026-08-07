3c4478212c4852c578b0c27d7ae818b8b000f250
Deymer Miranda <deymermiranda01@gmail.com>
2026-08-07 15:39:02 -0600

    feat(cloud 24/7): Publicar sw.js en miranda-connect repo

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Service Worker para Instalación Nativa PWA Miranda Connect 24/7
+const CACHE_NAME = "miranda-connect-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
